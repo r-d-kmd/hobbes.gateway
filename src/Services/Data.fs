a0aa2b0842876e198d8adc6c2836cfce14761db2
Rune Lund-Søltoft <rune@lundsoltoft>
2021-05-03 21:13:59 +0200

    Integration error between gateway and the new way of handling configurations and data in uniformdata

--- src/Services/Data.fs
+++ src/Services/Data.fs
@@ -10,32 +10,15 @@ module Data =
     let private cacheRevision confDoc = 
         sprintf "%s:%d" confDoc (System.DateTime.Now.Ticks) |> hash
 
-    //[<Post ("/json/", true)>]
-    //let graphql query = 
-    //  ()
-
     [<Get ("/json/%s")>]
     let json configuration =  
         debugf "Getting json for '%A'" configuration
         match Http.get (configuration |> Some |> Http.Configuration |> Http.Configurations) RawdataTypes.Config.Parse with
         Http.Success config -> 
-            let key = config |> RawdataTypes.keyFromConfig
-            match Http.get (key + ":Json" |> Http.UniformDataService.ReadFormatted |> Http.UniformData) id with
+            let key = config.Name
+            match Http.get (key |> Http.UniformDataService.Read |> Http.UniformData) id with
             Http.Success json ->
                 200, json
-            | Http.Error(404,m) -> 
-                debugf "Data for configuration %s not found. Message: %s" configuration m
-                match Http.get (key |> Http.UniformDataService.Read |> Http.UniformData) id with
-                Http.Success _ ->
-                    {
-                       Format = Json
-                       CacheKey = key
-                    }
-                    |> Format
-                    |> Broker.Calculation
-                    503, "{}"
-                | Http.Error(sc,m) -> 
-                    sc,sprintf "Data for configuration %s not found. Message: %s" configuration m
             | Http.Error(sc,m) ->
                 sc,sprintf "Failed retrieving data. Message: %s" m
         | Http.Error(sc,m) -> sc,sprintf "Configuration %s not found. Message: %s" configuration m
